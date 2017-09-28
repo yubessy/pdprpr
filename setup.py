33c181b24f79a3b78bf48b1cddd3c91bd57a55fc
yubessy <yubessy0@gmail.com>
2017-09-28 13:40:45 +0900

    v0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pdprpr',
-    version='0.2.0',
+    version='0.3.0',
     author='Shotaro Tanaka',
     author_email='yubessy0@gmail.com',
     packages=['pdprpr', 'pdprpr.series'],
